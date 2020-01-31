8d6d636f5faddf867461484d94b3979c77594867
georgikrastev <georgi.krystev89@gmail.com>
2020-01-31 16:01:25 +0200

    Remove onfig

--- src/store/index.js
+++ src/store/index.js
@@ -7,20 +7,7 @@ import 'firebase/firestore';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer, createFirestoreInstance } from 'redux-firestore';
 
-const fbConfig = {
-	apiKey: 'AIzaSyBBPv8WVNZrzDdDrJUY-mOo2edoT3lgbhA',
-	authDomain: 'recipes-app-de2a9.firebaseapp.com',
-	databaseURL: 'https://recipes-app-de2a9.firebaseio.com',
-	projectId: 'recipes-app-de2a9',
-	storageBucket: 'recipes-app-de2a9.appspot.com',
-	messagingSenderId: '773335171097',
-	appId: '1:773335171097:web:eef84dff62e6d38c'
-};
-
-const rrfConfig = {
-	userProfile: 'users',
-	useFirestoreForProfile: true
-};
+import { fbConfig, rrfConfig } from '../config';
 
 firebase.initializeApp(fbConfig);
 firebase.firestore();
