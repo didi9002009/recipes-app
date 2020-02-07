8e9fd035abe4d6ec5c7166b523a00fac80e9c944
georgikrastev <georgi.krystev89@gmail.com>
2020-02-07 11:11:59 +0200

    Style Details component

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -23,43 +23,53 @@ class Details extends React.Component {
 				<div className='details__bar'></div>
 
 				<div className='details__head'>
-					<Link to={appHomeUrl} className='details__back'>
-						<i className='material-icons'>arrow_back</i>
-					</Link>
+					<div className='container'>
+						<div className='details__head-inner'>
+							<Link to={appHomeUrl} className='details__back'>
+								<i className='material-icons'>arrow_back</i>
+							</Link>
 
-					<h1 className='details__title'>{recipe.title}</h1>
+							<h1 className='details__title'>{recipe.title}</h1>
 
-					<ul className='details__meta'>
-						<li>
-							<i className='material-icons'>timer</i>
+							<ul className='details__meta'>
+								<li>
+									<i className='material-icons'>timer</i>
 
-							<span>
-								{recipe.timeToCook} {timeToCookText}
-							</span>
-						</li>
+									<span>
+										{recipe.timeToCook} {timeToCookText}
+									</span>
+								</li>
 
-						<li>
-							<i className='material-icons'>restaurant</i>
+								<li>
+									<i className='material-icons'>restaurant</i>
 
-							<span>
-								{recipe.portions} {portionsText}
-							</span>
-						</li>
-					</ul>
+									<span>
+										{recipe.portions} {portionsText}
+									</span>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 
 				<div className='details__body'>
-					<h3>{ingredientsTitle}</h3>
+					<div className='container'>
+						<div className='details__body-inner'>
+							<h3>{ingredientsTitle}</h3>
 
-					<ul>
-						{recipe.ingredients.split(',').map(ingredient => (
-							<li key={ingredient}>{ingredient}</li>
-						))}
-					</ul>
+							<ul>
+								{recipe.ingredients
+									.split(',')
+									.map(ingredient => (
+										<li key={ingredient}>{ingredient}</li>
+									))}
+							</ul>
 
-					<h3>{instructionsTitle}</h3>
+							<h3>{instructionsTitle}</h3>
 
-					<p>{recipe.instructions}</p>
+							<p>{recipe.instructions}</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
