ab717b9575e630f4854c2e40578fe00e56a2af81
georgikrastev <georgi.krystev89@gmail.com>
2020-02-18 12:31:49 +0200

    Update firebase config object

--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,19 @@ import { firestoreReducer, createFirestoreInstance } from 'redux-firestore';
 
 import appReducers from '../components/App/reducer';
 
-/* eslint-disable-next-line no-undef */
-firebase.initializeApp(process.env.REACT_APP_FIREBASE_CONF);
+/* eslint-disable no-undef */
+const fbConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(fbConfig);
 firebase.firestore();
 
 const rootReducer = combineReducers({
