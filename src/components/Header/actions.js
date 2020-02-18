1ceee12fa47efa03a03ee05f4fdc56b21840299a
georgikrastev <georgi.krystev89@gmail.com>
2020-02-18 16:47:38 +0200

    Revert to previous solution

--- src/components/Header/actions.js
+++ src/components/Header/actions.js
@@ -5,8 +5,7 @@ import { limitNumber } from '../List/constants';
 
 export const logOut = () => {
 	return dispatch => {
-		dispatch(updateRecipesLimit(limitNumber));
 		firebase.auth().signOut();
-		firebase.logout();
+		dispatch(updateRecipesLimit(limitNumber));
 	};
 };
