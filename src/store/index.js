f9762591e38c333ada124d40ac1c477a385d6fcb
georgikrastev <georgi.krystev89@gmail.com>
2020-02-18 18:25:05 +0200

    Remove redux trace

--- src/store/index.js
+++ src/store/index.js
@@ -31,9 +31,7 @@ const rootReducer = combineReducers({
 });
 
 const composeEnchancers =
-	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-		trace: true
-	}) || compose;
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
 	rootReducer,
